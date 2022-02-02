2b3be6932aee4f1ab532a82202e27f35537fbbc0
Max <maximzakharov5@gmail.com>
2022-02-02 22:24:35 +0300

    Add choice city option (bad realization).

--- meteoservice.rb
+++ meteoservice.rb
@@ -5,7 +5,30 @@ require 'rexml/document'
 # Подключаем класс MeteoserviceForecast
 require_relative 'lib/meteoservice_forecast'
 
-URL = 'https://www.meteoservice.ru/export/gismeteo/point/112.xml'.freeze
+city = 0
+
+puts "Погоду для какого города Вы хотите узнать?"
+puts "
+1: Москва
+2: Санкт-Петербург
+3: Новосибирск
+4: Белгород
+5: Чита
+6: Братск
+7: Краснодар" 
+
+#URL = 'https://www.meteoservice.ru/export/gismeteo/point/112.xml'.freeze
+
+user_choice = STDIN.gets.chomp.to_i
+if user_choice != '' && user_choice != nil
+  city = MeteoserviceForecast::CITIES[user_choice - 1]
+end
+indx = user_choice.to_i
+
+str = MeteoserviceForecast::Urla[indx -1]
+
+URL = "https://www.meteoservice.ru/export/gismeteo/point/#{MeteoserviceForecast::Urla[user_choice]}.xml"
+
 
 response = Net::HTTP.get_response(URI.parse(URL))
 doc = REXML::Document.new(response.body)
@@ -15,9 +38,8 @@ city_name = URI.decode_www_form_component(doc.root.elements['REPORT/TOWN'].attri
 # Достаем все XML-теги <FORECAST> внутри тега <TOWN> и преобразуем их в массив
 forecast_nodes = doc.root.elements['REPORT/TOWN'].elements.to_a
 
-# Выводим название города и все прогнозы по порядку
-puts city_name
-puts
+
+puts city 
 
 forecast_nodes.each do |node|
   puts MeteoserviceForecast.from_xml(node)
